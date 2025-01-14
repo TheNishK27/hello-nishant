00fee65de3584aeaa994072e9b18fdcef58c6079
Nishant Kumar <143710192+TheNishK27@users.noreply.github.com>
2025-01-14 10:59:51 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,21 @@ const Hero = () => {
         <div className="relative">
             <div className='font-bold text-center'>
                 <h1 className="text-[#98B4CE] text-4xl md:text-7xl">Hi, I am</h1>
-                <h1 className="text-orange-400 md:text-[#E48A57] text-5xl md:text-8xl">Nishant Kumar</h1>
+                <h1 className="text-orange-400 md:text-[#E48A57] text-5xl md:text-8xl font-semibold flex justify-center bubble-text">
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">N</span>
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">i</span>
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">s</span>
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">h</span>
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">a</span>
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">n</span>
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">t</span>
+                    <span className="split-char">&nbsp;</span>
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">K</span>
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">u</span>
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">m</span>
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">a</span>
+                    <span className="split-char transition-transform duration-500 hover:scale-110 hover:text-orange-500 hover:font-bold">r</span>
+                </h1>
             </div>
             <motion.div 
             className="hidden md:block absolute left-[260px] top-[240px]" 
